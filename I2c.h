7f4217b45f7ce0219f1b79178e185937fb65efa7
Hyeonki Hong <hhk7734@gmail.com>
2019-10-22 23:16:57 +0900

    API: Add constructor and destructor

--- I2c.h
+++ I2c.h
@@ -30,6 +30,9 @@ namespace lot
 class I2c : public II2c
 {
 public:
+    I2c( uint16_t bus_num );
+    ~I2c();
+
     void    init( uint32_t clock );
     void    set_clock( uint32_t clock );
     void    transmit( uint8_t slave_address, uint8_t *buffer, uint16_t size );
